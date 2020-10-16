dbba1e11aaaac2fb1b1451e784bdf75949aaedcf
bhryck <randall2000n@gmail.com>
2020-10-16 15:02:15 +0800

    Update lab6.py

--- ai/ass2/lab6.py
+++ ai/ass2/lab6.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 """ 
 Group member: 
 Michelle Karen Natasya Putri SANDHIKA 18078659d
@@ -8,9 +9,11 @@ Nicholas Matthew KURNIADI 18079267D
 """
 
 
-
 np.random.seed(42)
 m = 200
 X = np.random.rand(m, 1)
 y = 4 * (X - 0.5) ** 2
 Y = y + np.random.randn(m, 1) / 10
+
+plt.scatter(X,Y)
+plt.show()
